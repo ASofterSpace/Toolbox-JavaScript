7efe599e32d275272a8675cd37a0efdff8ce3371
Tom Moya Schau <moya@asofterspace.com>
2023-05-04 08:47:55 +0200

    Add generic code for handling function keys in textareas

--- toolbox/utils/StrUtils-0.0.1.js
+++ toolbox/utils/StrUtils-0.0.1.js
@@ -63,6 +63,63 @@ var StrUtils = {
 		return result;
 	},
 
+	// insert generic text to an open textarea based on an event
+	insertText: function(textarea, toInsert, event) {
+		var start = textarea.selectionStart;
+		var end = textarea.selectionEnd;
+		textarea.value =
+			textarea.value.substring(0, start) +
+			toInsert +
+			textarea.value.substring(end);
+		textarea.selectionStart = start + toInsert.length;
+		textarea.selectionEnd = start + toInsert.length;
+	},
+
+	// add a date-time-stamp to an open textarea based on an event
+	addDateTimeStamp: function(textarea, event) {
+		var datetimestamp = toolbox.utils.DateUtils.getCurrentDateTimeStamp();
+		this.insertText(textarea, datetimestamp, event);
+	},
+
+	// indent or unindent several lines in an open textarea based on an event
+	indentOrUnindent(textarea, event) {
+		var start = textarea.selectionStart;
+		var end = textarea.selectionEnd;
+		var val = textarea.value;
+		// if this comes back as -1, we add 1 and are at 0, which is exactly what we want :)
+		start = val.lastIndexOf("\n", start) + 1;
+		var indented = val.substring(start, end).split("\n");
+		for (var i = 0; i < indented.length; i++) {
+			if (event.shiftKey) {
+				// un-indent
+				if (indented[i].substring(0, 1) == "\t") {
+					indented[i] = indented[i].substring(1);
+				} else if (indented[i].substring(0, 4) == "    ") {
+					indented[i] = indented[i].substring(4);
+				} else if (indented[i].substring(0, 3) == "   ") {
+					indented[i] = indented[i].substring(3);
+				} else if (indented[i].substring(0, 2) == "  ") {
+					indented[i] = indented[i].substring(2);
+				} else if (indented[i].substring(0, 1) == " ") {
+					indented[i] = indented[i].substring(1);
+				}
+			} else {
+				// indent
+				if (indented[i].trim() != "") {
+					indented[i] = "\t" + indented[i];
+				}
+			}
+		}
+		var indentedVal = indented.join("\n");
+		textarea.value =
+			val.substring(0, start) +
+			indentedVal +
+			val.substring(end);
+		textarea.selectionStart = start;
+		textarea.selectionEnd = start + indentedVal.length;
+	},
+
+
 };
 
 
